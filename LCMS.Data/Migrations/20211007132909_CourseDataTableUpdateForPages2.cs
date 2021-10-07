d431f36e01de30c03c5d6dfe390c7848d7e85d9d
James Avery Wilhelm <hello@jamesaverywilhelm.com>
2021-10-07 09:29:54 -0400

    Removed extra column from course db

---
+++ LCMS.Data/Migrations/20211007132909_CourseDataTableUpdateForPages2.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LCMS.Data.Migrations
+{
+    public partial class CourseDataTableUpdateForPages2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pages_Courses_CourseCrs_Id",
+                table: "Pages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Pages_CourseCrs_Id",
+                table: "Pages");
+
+            migrationBuilder.DropColumn(
+                name: "CourseCrs_Id",
+                table: "Pages");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CourseCrs_Id",
+                table: "Pages",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pages_CourseCrs_Id",
+                table: "Pages",
+                column: "CourseCrs_Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pages_Courses_CourseCrs_Id",
+                table: "Pages",
+                column: "CourseCrs_Id",
+                principalTable: "Courses",
+                principalColumn: "Crs_Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
